4a2c1b576de8631b543842d73707c621b7e76d64
Julian Bender <julian.bender@uni-wuerzburg.de>
2023-06-16 16:02:12 +0200

    Updated cran source

--- mypackage/RFunctions.R
+++ mypackage/RFunctions.R
@@ -1,14 +1,14 @@
 # Function to test installation status and install packages from CRAN
 CRANpkgTest <- function(x)
 {
-  if (!require("pak",character.only = TRUE))
+  if (!require(x,character.only = TRUE))
   {
-    install.packages("pak",dep=TRUE, repos=c('https://ftp.fau.de/cran/', 'https://cloud.r-project.org'))
+    install.packages(x,dep=TRUE, repos=c('https://ftp.fau.de/cran/', 'https://cloud.r-project.org'))
     if(!require(x,character.only = TRUE)) stop("Package not found")
   }
 }
 
-CRAN_packages <- c("rrcovNA", "tidyverse", "BiocManager", "devtools")
+CRAN_packages <- c("pak", "rrcovNA", "tidyverse", "BiocManager", "devtools")
 for (package in CRAN_packages){
   CRANpkgTest(package)
 }
