c1c8f67e90c7eb3ff8fba531e118d9c643126086
Julian Bender <julian.bender@uni-wuerzburg.de>
2023-06-16 22:14:18 +0200

    Moved back to devtools

--- mypackage/RFunctions.R
+++ mypackage/RFunctions.R
@@ -7,25 +7,25 @@ if(!require("pak",character.only = TRUE)) stop("Package not found")
 
 # Collect all required packages as vectors and install with pak
 # see https://pak.r-lib.org/dev/reference/get-started.html
-CRAN_packages <- c("rrcovNA", "tidyverse")
+CRAN_packages <- c("rrcovNA", "tidyverse", "devtools")
 BC_packages <- c("limma", "vsn", "RankProd", "pcaMethods", "impute", "SummarizedExperiment")
 github_packages <- c("cran/DMwR", "kreutz-lab/DIMAR")
 URL_install <- c("url::https://cran.r-project.org/src/contrib/Archive/imputation/imputation_1.3.tar.gz")
 
-for (package in c(github_packages, CRAN_packages, BC_packages, URL_install)){
+for (package in c(URL_install, CRAN_packages, BC_packages)){
     pak::pkg_install(package)
 }
 
-# for (package in github_packages){
-#   installedPackges <- rownames(installed.packages())
-#   pkgName <- strsplit(package, "/")[[1]][[2]]
-#   if (pkgName %in% installedPackges){
-#     require(pkgName,character.only = TRUE)
-#   }
-#   else {
-#     devtools::install_github(package)
-#   }
-# }
+for (package in github_packages){
+  installedPackges <- rownames(installed.packages())
+  pkgName <- strsplit(package, "/")[[1]][[2]]
+  if (pkgName %in% installedPackges){
+    require(pkgName,character.only = TRUE)
+  }
+  else {
+    devtools::install_github(package)
+  }
+}
 
 # read the args coming form Python
 args = commandArgs(trailingOnly=TRUE)
