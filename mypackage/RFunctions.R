336b6728499d1241fda783f31d3acdf3bcd82d9f
Julian Bender <julian.bender@uni-wuerzburg.de>
2023-06-16 17:22:26 +0200

    Added all autoprot packages

--- mypackage/RFunctions.R
+++ mypackage/RFunctions.R
@@ -1,18 +1,23 @@
-# Function to test installation status and install packages from CRAN
+# First install pak with the standard installation routine
+if (!require("pak",character.only = TRUE))
 {
-  if (!require("pak",character.only = TRUE))
-  {
-    install.packages("pak",dep=TRUE, repos=c('https://ftp.fau.de/cran/', 'https://cloud.r-project.org'))
-    if(!require("pak",character.only = TRUE)) stop("Package not found")
-  }
+install.packages("pak",dep=TRUE, repos=c('https://ftp.fau.de/cran/', 'https://cloud.r-project.org'))
+if(!require("pak",character.only = TRUE)) stop("Package not found")
 }
 
-CRAN_packages <- c("rrcovNA", "tidyverse", "BiocManager", "devtools")
-for (package in CRAN_packages){
-    if (!require("pak",character.only = TRUE)){
-      pak::pkg_install(package)
-      if(!require("pak",character.only = TRUE)) stop("Package not found")
-    }
+# Collect all required packages as vectors and install with pak
+# see https://pak.r-lib.org/dev/reference/get-started.html
+CRAN_packages <- c("rrcovNA", "tidyverse")
+BC_packages <- c("limma", "vsn", "RankProd", "pcaMethods", "impute", "SummarizedExperiment")
+github_packages <- c("cran/DMwR", "kreutz-lab/DIMAR")
+URL_install <- c("url::https://cran.r-project.org/src/contrib/Archive/imputation/imputation_1.3.tar.gz")
+
+for (package in c(CRAN_packages, BC_packages, github_packages){
+  if (!require(x,character.only = TRUE))
+  {
+    pak::pkg_install(x)
+    if(!require(x,character.only = TRUE)) stop("Package not found")
+  }
 }
 
 # read the args coming form Python
