b9fce4563bca57bc0e5567579835b9343d96f71c
Julian Bender <julian.bender@uni-wuerzburg.de>
2023-06-16 18:33:59 +0200

    removed github packages from pak as it requires git credentials I cannot supply

--- mypackage/RFunctions.R
+++ mypackage/RFunctions.R
@@ -7,17 +7,28 @@ if(!require("pak",character.only = TRUE)) stop("Package not found")
 
 # Collect all required packages as vectors and install with pak
 # see https://pak.r-lib.org/dev/reference/get-started.html
-CRAN_packages <- c("rrcovNA", "tidyverse")
+CRAN_packages <- c("rrcovNA", "tidyverse", "devtools")
 BC_packages <- c("limma", "vsn", "RankProd", "pcaMethods", "impute", "SummarizedExperiment")
-github_packages <- c("cran/DMwR", "kreutz-lab/DIMAR")
 URL_install <- c("url::https://cran.r-project.org/src/contrib/Archive/imputation/imputation_1.3.tar.gz")
 
-for (package in c(CRAN_packages, BC_packages, github_packages)){
+for (package in c(CRAN_packages, BC_packages, URL_install)){
   if (!require(package,character.only = TRUE)){
     pak::pkg_install(package)
     if(!require(package,character.only = TRUE)){
       stop("Package not found")
     }
+  }
+}
+
+github_packages <- c("cran/DMwR", "kreutz-lab/DIMAR")
+for (package in github_packages){
+  installedPackges <- rownames(installed.packages())
+  pkgName <- strsplit(package, "/")[[1]][[2]]
+  if (pkgName %in% installedPackges){
+    require(pkgName,character.only = TRUE)
+  }
+  else {
+    devtools::install_github(package)
   }
 }
 
