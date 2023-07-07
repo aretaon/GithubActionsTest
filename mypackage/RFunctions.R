4feaf5d05592aad3b07e1b39721aaf66faba73de
Julian Bender <julian.bender@uni-wuerzburg.de>
2023-07-07 17:06:47 +0200

    new routine to skip installed packages

--- mypackage/RFunctions.R
+++ mypackage/RFunctions.R
@@ -1,5 +1,3 @@
-install.packages("tmvtnorm", dep=TRUE, repos=c('https://ftp.fau.de/cran/', 'https://cloud.r-project.org'))
-
 # First install pak with the standard installation routine
 if (!require("pak",character.only = TRUE))
 {
@@ -14,8 +12,15 @@ BC_packages <- c("limma", "vsn", "RankProd", "pcaMethods", "impute", "Summarized
 github_packages <- c("cran/DMwR", "kreutz-lab/DIMAR")
 URL_install <- c("url::https://cran.r-project.org/src/contrib/Archive/imputation/imputation_1.3.tar.gz")
 
+installedPackges <- rownames(installed.packages())
 for (package in c(URL_install, github_packages, CRAN_packages, BC_packages)){
+  if (x %in% installedPackges){
+    require(x,character.only = TRUE)
+    }
+  else{
     pak::pkg_install(package)
+    require(x,character.only = TRUE)
+    }
 }
 
 # read the args coming form Python
