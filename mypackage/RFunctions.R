19bcf02fbf7f50e3daad3bf3ba5ef4ba67f9cd35
Julian Bender <julian.bender@uni-wuerzburg.de>
2023-07-07 16:45:13 +0200

    Updated RFunctinos

--- mypackage/RFunctions.R
+++ mypackage/RFunctions.R
@@ -1,3 +1,5 @@
+install.packages("tmvtnorm", dep=TRUE, repos=c('https://ftp.fau.de/cran/', 'https://cloud.r-project.org'))
+
 # First install pak with the standard installation routine
 if (!require("pak",character.only = TRUE))
 {
