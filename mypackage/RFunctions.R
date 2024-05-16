950d063a23c8f40384f42c5eeb3599aa0b9de22c
Julian Bender <julian.bender@uni-wuerzburg.de>
2024-05-16 15:54:22 +0200

    Updated workflow and RFunctions.R

--- mypackage/RFunctions.R
+++ mypackage/RFunctions.R
@@ -9,11 +9,10 @@ if(!require("pak",character.only = TRUE)) stop("Package not found")
 # see https://pak.r-lib.org/dev/reference/get-started.html
 CRAN_packages <- c("rrcovNA", "tidyverse", "tmvtnorm")
 BC_packages <- c("limma", "vsn", "RankProd", "pcaMethods", "impute", "SummarizedExperiment")
-github_packages <- c("github::cran/DMwR", "github::kreutz-lab/DIMAR")
-URL_install <- c("imputation=url::https://cran.r-project.org/src/contrib/Archive/imputation/imputation_1.3.tar.gz")
+github_packages <- c("github::kreutz-lab/DIMAR")
 
 installedPackages <- rownames(installed.packages())
-for (package in c(URL_install, github_packages, CRAN_packages, BC_packages)){
+for (package in c(github_packages, CRAN_packages, BC_packages)){
   if (package %in% installedPackages){
     require(package, character.only = TRUE)
     }
@@ -24,7 +23,7 @@ for (package in c(URL_install, github_packages, CRAN_packages, BC_packages)){
 }
 
 # read the args coming form Python
-args = commandArgs(trailingOnly=TRUE)
+args <- commandArgs(trailingOnly=TRUE)
 
 input <- args[1]
 print(input)
